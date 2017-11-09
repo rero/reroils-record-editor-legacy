46f5bf42d92915753938f010f9397044d2c78ae5
Philippe Roud <philippe.roud@rero.ch>
2017-11-09 08:27:05 +0100

    feat : add a Jenkins pipe for the editor
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

---
+++ development/build_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+echo "Building version: $1"
+source /reroils/reroils/bin/activate && \
+cd reroils/src && \
+git clone https://gitlab.rero.ch/rero21/reroils-record-editor.git && \
+cd reroils-record-editor; 
+pip install -r requirements.txt
+pip install .[all]
+
+
---
+++ development/test.sh
@@ -0,0 +1,3 @@
+source reroils/bin/activate
+cd /reroils/reroils/src/reroils-record-editor
+./run-tests.sh
