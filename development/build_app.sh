e6419bac59f0e1f45ac80b66f2d673f374824ca6
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2017-11-21 15:54:05 +0100

    feat: update translations
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- development/build_app.sh
+++ development/build_app.sh
@@ -4,7 +4,7 @@ echo "Building version: $1"
 source /reroils/reroils/bin/activate && \
 cd reroils/src && \
 git clone https://gitlab.rero.ch/rero21/reroils-record-editor.git && \
-cd reroils-record-editor; 
+cd reroils-record-editor;
 pip install -r requirements.txt
 pip install .[all]
 
