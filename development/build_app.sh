5ee39016af5c34f6cfd4454167320c5a2f65ca2f
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2018-02-21 08:03:30 +0100

    refact: move to github
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- development/build_app.sh
+++ development/build_app.sh
@@ -3,7 +3,7 @@
 echo "Building version: $1"
 source /reroils/reroils/bin/activate && \
 cd reroils/src && \
-git clone https://gitlab.rero.ch/rero21/reroils-record-editor.git && \
+git clone https://github.com/rero/reroils-record-editor.git && \
 cd reroils-record-editor;
 pip install -r requirements.txt
 pip install .[all]
