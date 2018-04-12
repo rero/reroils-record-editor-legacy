f3e45b049c288ddb077af2410780fbee60e58269
Igor Milhit <igor.milhit@rero.ch>
2018-04-12 11:48:22 +0200

    fix: fix #1 to clean copyright headers

--- reroils_record_editor/static/js/reroils_record_editor/reroils-editor.js
+++ reroils_record_editor/static/js/reroils_record_editor/reroils-editor.js
@@ -1,9 +1,6 @@
-
-
-
 /*
  * This file is part of Invenio.
- * Copyright (C) 2015, 2016 CERN.
+ * Copyright (C) 2017 RERO.
  *
  * Invenio is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -19,7 +16,7 @@
  * along with Invenio; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
- * In applying this license, CERN does not
+ * In applying this license, RERO does not
  * waive the privileges and immunities granted to it by virtue of its status
  * as an Intergovernmental Organization or submit itself to any jurisdiction.
  */
